eafe6b910ff1f18434627bf4e4db9810b23e4b89
munlover <munlove@mirai.re>
2022-03-08 20:14:51 +0900

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-<a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
-  <span aria-hidden="true"></span>
-  <span aria-hidden="true"></span>
-  <span aria-hidden="true"></span>
-</a>
-
-<div class="navbar-menu" id="navMenu">
-  <!-- navbar-start, navbar-end... -->
-</div>
